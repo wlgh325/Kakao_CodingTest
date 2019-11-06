434f506ec2b3139fbd97f89dcbe9202ec57b480b
jiho <wlgh325@naver.com>
2019-11-06 13:23:29 +0900

    solve the problem

--- 2020_BlindTest/문자열 압축/Main.java
+++ 2020_BlindTest/문자열 압축/Main.java
@@ -1,19 +1,76 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 class Main {
-	public static void main(String[] args) throws IOException{
-        String p1 = "(()())()";
-        String p2 = ")(";
-        String p3 = "()))((()";
+        static ArrayList<String> arrList = new ArrayList<>();
 
-        String answer = "";
+	public static void main(String[] args){
+                String p1 = "aabbaccc";
+                //String p2 = "ababcdcdababcdcd";
+                //String p3 = "abcabcdede";
+                //String p4 = "abcabcabcabcdededededede";
+                //String p5 = "xababcdcdababcdcd";
+                //String p6 = "a";
 
-
-        System.out.println(answer);
-		
+                int min = solve(p1);
+                System.out.println(min);
 	}
 
+        public static int solve(String s){
+                String answer = "";
+                
+                // 문자열 길이가 1일 경우 예외처리
+                if(s.length() == 1){
+                        return 1;
+                }
+
+                // i: 몇개 씩 묶을지
+                for (int i = 1; i < s.length()/2 + 1; i++) {
+                        int len = 1;
+                        String before = s.substring(0, i);
+                        int temp = s.length() /i;
+
+                        int j=i;
+                        for(; j<i*temp;){
+                                String next = s.substring(j, j+i);
+                                
+                                if(before.equals(next)){
+                                    len++;    
+                                }
+                                else{
+                                        if(len != 1){
+                                                answer += Integer.toString(len);
+                                                answer += before;
+                                        }
+                                        else{
+                                                answer += before;
+                                        }
+                                        len = 1;
+                                }
+                                //System.out.println("before: " + before + " next: " + next);
+                                before = next;
+                                j= j+i;
+                        }
+
+                        if(len != 1){
+                                answer += Integer.toString(len);
+                                answer += s.substring(j-i, s.length());
+                        }
+                        else{
+                                answer += s.substring(j-i, s.length());
+                        }
+                        //System.out.println("answer: " + answer);
+                        arrList.add(answer);
+                        answer = "";
+
+                }
+
+                int min = 999999;
+                for (String str : arrList) {
+                        int len = str.length();
+                        if(min >= len)
+                                min = len;
+                }
+
+                return min;
+        }
 }
