3d81cc166ae42dfb3cc0a62716280ec2ebf9c5fc
wlgh325 <wlgh325@gmail.com>
2019-11-06 18:09:28 +0900

    third problem start

---
+++ 2020_BlindTest/자물쇠와 열쇠/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+class Main {
+	public static void main(String[] args){
+        String p1 = "(()())()";
+        String p2 = ")(";
+        String p3 = "()))((()";
+
+        String p4 = "";
+
+        String answer = solution(p3);
+        System.out.println(answer);
+    }
+    
+    public static String solution(String p){
+        String answer = "";
+
+        return answer;
+    }
+
+}
