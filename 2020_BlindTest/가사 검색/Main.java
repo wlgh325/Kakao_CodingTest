fd3c530d877c2220008b64e81559f6d65caebc81
jiho <wlgh325@naver.com>
2019-11-08 02:55:28 +0900

    solve problem except accuracy

--- 2020_BlindTest/가사 검색/Main.java
+++ 2020_BlindTest/가사 검색/Main.java
@@ -1,31 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Main{
     public static void main(String[] args){
         String[] words = {"frodo", "front", "frost", "frozen", "frame", "kakao"};
         String[] queries = {"fro??", "????o", "fr???", "fro???", "pro?"};
 
-        boolean answer = solution(words, queries);
+        int[] answer = solution(words, queries);
     }
 
-    public static boolean solution(String[] words, String[] queries){
-        int[] answer = {};
-
+    public static int[] solution(String[] words, String[] queries){
+        int[] answer = new int[queries.length];
+        ArrayList<String> queryList = new ArrayList<>(Arrays.asList(queries));
+        queries = queryList.toArray(new String[queryList.size()]);
+        
+        Arrays.sort(words);
+        
         for (int i = 0; i < queries.length; i++) {
             String query = queries[i];
             int query_len = query.length();
             int num =0;
 
             for (int j = 0; j < words.length; j++) {
-                String word = words[j]
+                String word = words[j];
                 int word_len = word.length();
 
                 if(word_len != query_len)
                     continue;
                 else{
-                    int idx = word.indexOf("?");
-                    
+                    int idx = query.indexOf("?");
+                    if(idx != 0){
+                        String sub = query.substring(0, idx);
+                        if(sub.equals(word.substring(0, idx)))
+                            num++;
+                    }
+                    else{
+                        int lastIdx = query.lastIndexOf("?");
+                        String sub = query.substring(lastIdx+1, query.length());
+                        if(sub.equals(word.substring(lastIdx +1, word.length())))
+                            num++;
+                    }
                 }
 
             }
+            answer[i] = num;
         }
 
 
