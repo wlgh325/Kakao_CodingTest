6964f0badc5ff427ab7640a7b5a0fb0f8f61459e
jiho <wlgh325@naver.com>
2019-11-08 00:55:08 +0900

    partial solve

--- 2020_BlindTest/자물쇠와 열쇠/Main.java
+++ 2020_BlindTest/자물쇠와 열쇠/Main.java
@@ -1,21 +1,99 @@
-import java.util.ArrayList;
-
 class Main {
 	public static void main(String[] args){
-        String p1 = "(()())()";
-        String p2 = ")(";
-        String p3 = "()))((()";
+        int[][] key = { {0,0,0},
+                        {1,0,0},
+                        {0,1,1}};
 
-        String p4 = "";
+        int[][] lock = { {1,1,1},
+                        {1,1,0},
+                        {1,0,1}};
 
-        String answer = solution(p3);
+        boolean answer = solution(key, lock);
         System.out.println(answer);
     }
     
-    public static String solution(String p){
-        String answer = "";
+    
+    public static boolean solution(int[][] key, int[][] lock){
+        boolean answer = false;
 
+        int lock_size = lock.length;
+
+        
+        int[][] bigLock = new int[lock_size*2 + lock_size-2][lock_size*2 + lock_size - 2];
+        int diff = lock_size - key.length;
+
+        // 크게 확장한 열쇠 만들기
+        for (int i = 0; i < lock_size; i++) {
+            for (int j = 0; j < lock_size; j++) {
+                bigLock[i+lock_size-1][j+lock_size-1] += lock[i][j];
+            }
+        }
+
+
+        // 열쇠와 자물쇠 맞춰보기
+        for (int w = 0; w< lock_size*2 + diff - 1; w++) {
+            for (int l = 0; l < lock_size*2 + diff - 1; l++) {
+                for (int k = 0; k < 3; k++) {
+                    int[][] copyLock = deepCopy(bigLock);
+        
+                    for (int i = 0; i < key.length; i++) {
+                        for (int j = 0; j < key.length; j++) {
+                            copyLock[i+w][j+l] += key[i][j];
+                        }
+                    }    
+        
+                    if(!isOpen(lock_size, copyLock)){
+                        key = rotate90(key);
+                    }
+                    else
+                        return true;
+                    
+                }
+                key = rotate90(key);
+            }
+        }
+        
         return answer;
     }
 
+    public static int[][] deepCopy(int[][] origin){
+        if(origin == null) return null;
+
+        int[][] dest = new int[origin.length][origin[0].length];
+
+        for (int i = 0; i < origin.length; i++) {
+            System.arraycopy(origin[i], 0, dest[i], 0, origin[0].length);
+        }
+
+        return dest;
+    }
+
+    public static int[][] rotate90(int[][] arr){
+        int[][] rotated = new int [arr.length][arr.length];
+
+        int len = arr.length;
+        for (int i = 0; i < rotated.length; i++) {
+            for (int j = 0; j < rotated.length; j++) {
+                rotated[i][j] = arr[len-j-1][i];
+            }
+        }
+
+        return rotated;
+    }
+
+    
+    //lock의 모든 부분이 1이되면 됨
+    public static boolean isOpen(int size, int[][] arr){
+        boolean flag = true;
+
+        for (int i =0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if(arr[i+size-1][j+size-1] == 0 || arr[i+size-1][j+size-1] == 2){
+                    return false;
+                }
+            }   
+        }
+
+        return flag;
+    }
 }
