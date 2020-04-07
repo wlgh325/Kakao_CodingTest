d3cbe48b456a1436c71114e18ea1b8cb742888fb
Jiho <wlgh325@naver.com>
2020-04-07 18:02:42 +0900

    solve 기둥과 보 (구현)

--- 2020 카카오 블라인드 채용 코딩 테스트/기둥과 보 설치/Main.java
+++ 2020 카카오 블라인드 채용 코딩 테스트/기둥과 보 설치/Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 
 class Info {
     int x;
@@ -13,8 +12,8 @@ class Info {
     }
 }
 
-class Main{
-    public static void main(String[] args){
+class Main {
+   public static void main(String[] args){
         int n = 5;
         int[][] build_frame = {{1,0,0,1},
         {1,1,1,1},
@@ -38,153 +37,120 @@ class Main{
         {2,2,0,1}};
 
         int[][] answer = solution(n, build_frame2);
-    }
-
-
-    // return : [x, y, a]
-
-    
-    static int[][][] map;
-    static ArrayList<Info> result;
-
-    public static int[][] solution(int n, int[][] build_frame) {        
-        result = new ArrayList<>();
-
-        map = new int[n+1][n+1][2];
-        // build_frame = {x, y, 종류, 설치/삭제}
-        for (int i = 0; i < build_frame.length; i++) {
-            int x = build_frame[i][0];
-            int y = build_frame[i][1];
-            int kinds = build_frame[i][2];
-            int menu = build_frame[i][3];
-
-            // 1: 설치
-            if(menu == 1){
-                if(isEstablishment(x, y, kinds)){
-                    map[x][y][kinds] = 1;
-                    Info info = new Info(x,y,kinds);
-                    result.add(info);
-                }
-                    
-            }
-            else if(menu == 0){
-                if(isRemove(x,y,kinds)){
-                    int idx = findInfo(x, y, kinds);
-                    result.remove(idx);
-                }
-                else 
-                    map[x][y][kinds] = 1;                
-            }
+        for(int i=0; i<answer.length; i++) {
+        	for(int j=0; j<3; j++) {
+        		System.out.print(answer[i][j] + " ");
+        	}
+        	System.out.println();
         }
+   }
+   
+   static int[][][] map;
+   public static int[][] solution(int n, int[][] build_frame) {
+	   map = new int[n+1][n+1][2];
+       // [x,y,a,b] : [좌표x, 좌표y, 구조물 종류, 설치/삭제]
+       // 구조물은 오른쪽으로, 기둥은 위쪽으로 설치/삭제
+       
+       // n개 명령 수행
+       for(int i=0; i<build_frame.length; i++) {
+    	   int x = build_frame[i][0];
+    	   int y = build_frame[i][1];
+    	   int kinds = build_frame[i][2];
+    	   int cmd = build_frame[i][3];
+    	   
+    	   // 1: 설치
+           if(cmd == 1){
+               if(isEstablishment(x, y, kinds))
+                   map[x][y][kinds] = 1;
+           }
+           else if(cmd == 0){
+        	   // 제거 가능한지 확인
+               if(isRemove(x,y,kinds))
+            	   map[x][y][kinds] = 0;
+               else 
+                   map[x][y][kinds] = 1;                
+           }
+       }
+       
+       ArrayList<Info> list = new ArrayList<>();
+       // answer 배열에 담을 설치된 구조물들 list에 추가
+       for(int i=0; i<n+1; i++) {
+    	   for(int j=0; j<n+1; j++) {
+    		   if(map[i][j][0] == 1)
+    			   list.add(new Info(i,j,0));
+    		   if(map[i][j][1] == 1)
+    			   list.add(new Info(i,j,1));
+    	   }
+       }
+       // answer 배열에 담기
+       int[][] answer = new int[list.size()][3];
+       for(int i=0; i<list.size(); i++) {
+    	   Info p = list.get(i);
+    	   answer[i][0] = p.x;
+    	   answer[i][1] = p.y;
+    	   answer[i][2] = p.kinds;
+       }
+       return answer;
+   }
+   
+   public static boolean isRemove(int x, int y, int kinds){
+       boolean flag = true;
+       
+       // 일단 제거
+       map[x][y][kinds] = 0;
+       
+       // 제거한 뒤에 지금 있는 장애물들이 설치가 가능한지 검사
+       // 다시 설치가 가능하다면 제거해도 문제가 없다!
+       for (int i = 0; i < map.length; i++) {
+           for (int j = 0; j < map.length; j++) {
+               for (int k = 0; k < 2; k++) {
+            	   // 장애물이 설치되어 있다면
+                   if(map[i][j][k]== 1){
+                	   // 제거해보기
+                       map[i][j][k] = 0;
+                       // 그 위치에 방금 제거한 장애물이 설치가 가능한지 확인
+                       if(!isEstablishment(i, j, k)){
+                           map[i][j][k] = 1;
+                           return false;
+                       } 
+                       map[i][j][k] = 1;
+                   }
+               }
+           }
+       }
 
-        //sort
-        result.sort(new Comparator<Info>() {
-            @Override
-            public int compare(Info arg0, Info arg1){
-                int a = arg0.x;
-                int b = arg1.x;
-                if(a == b){
-                    int c = arg0.y;
-                    int d = arg1.y;
+       return flag;
+   }
 
-                    if(c == d){
-                        int e = arg0.kinds;
-                        int f = arg1.kinds;
-                        if(e == f)
-                            return 0;
-                        else if(e > f)
-                            return 1;
-                        else
-                            return -11;
-                    }
-                    else if(c > d)
-                        return 1;
-                    else
-                        return -1;
-                }
-                else if(a > b)
-                    return 1;
-                else
-                    return -1;
-            }
-        });
+   public static boolean isEstablishment(int x, int y, int kinds){
+       boolean flag = false;
+       // 보와 기둥을 나눠서 체크
+       switch(kinds){
+           case 0:
+               // 그 위치에 보가 있는 경우 보 위에 설치 가능
+               if(map[x][y][1] == 1)
+                   return true;
+               // 왼쪽에 보의 끝에 걸친 경우
+               if(x-1 >= 0 && map[x-1][y][1] == 1)
+                   return true;
+               // 바닥에 설치하는 경우
+               if(y == 0)
+                   return true;
+               // 다른 기둥 위에 있는 경우
+               if(y-1 >= 0 && map[x][y-1][0] == 1)
+                   return true;
 
-        int[][] answer = new int[result.size()][3];
-        
-        // 배열로 변환
-        for (int i = 0; i < result.size(); i++) {
-            answer[i][0] = result.get(i).x;
-            answer[i][1] = result.get(i).y;
-            answer[i][2] = result.get(i).kinds;
-        }
-        System.out.println("h");
-        return answer;
-    }
+               break;
+           // 보를 세우기
+           case 1:
+               // 한쪽 끝이 기둥 위에 있는 경우
+               if(y-1 >= 0 && (map[x][y-1][0] == 1 || map[x+1][y-1][0] == 1))
+                   return true;
+               // 양쪽 끝 부분이 다른 보와 동시에 연결되어 있는 경우
+               if(x-1 >= 0 && map[x-1][y][1] == 1 && map[x+1][y][1] == 1)
+                   return true;
 
-    public static boolean isRemove(int x, int y, int kinds){
-        boolean flag = true;
-
-        map[x][y][kinds] = 0;
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map.length; j++) {
-                for (int k = 0; k < 2; k++) {
-                    if(map[i][j][k]== 1){
-                        map[i][j][k] = 0;
-                        if(!isEstablishment(i, j, k)){
-                            map[i][j][k] = 1;
-                            return false;
-                        } 
-                        map[i][j][k] = 1;
-                    }
-                }
-            }
-        }
-
-        return flag;
-    }
-
-    public static int findInfo(int x, int y, int kinds){
-        int idx = -1;
-
-        for (int i = 0; i < result.size(); i++) {
-            Info info = result.get(i);
-            if(info.x == x && info.y == y && info.kinds == kinds){
-                idx = i;
-                break;
-            }
-        }
-        return idx;
-    }
-
-    public static boolean isEstablishment(int x, int y, int kinds){
-        boolean flag = false;
-
-        switch(kinds){
-            case 0:
-                // 보가 있는 경우
-                if(map[x][y][1] == 1)
-                    return true;
-                // 왼쪽에 보의 끝에 걸친 경우
-                if(x-1 >= 0 && map[x-1][y][1] == 1)
-                    return true;
-                // 바닥에 설치하는 경우
-                if(y == 0)
-                    return true;
-                // 다른 기둥 위에 있는 경우
-                if(y-1 >= 0 && map[x][y-1][0] == 1)
-                    return true;
-
-                break;
-            // 보를 세우기
-            case 1:
-                // 한쪽 끝이 기둥 위에 있는 경우
-                if(y-1 >= 0 && (map[x][y-1][0] == 1 || map[x+1][y-1][0] == 1))
-                    return true;
-                // 양쪽 끝 부분이 다른 보와 동시에 연결되어 있는 경우
-                if(x-1 >= 0 && map[x-1][y][1] == 1 && map[x+1][y][1] == 1)
-                    return true;
-
-        }
-        return flag;
-    }
+       }
+       return flag;
+   }
 }
