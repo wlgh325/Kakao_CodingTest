058520e2c54696e153e6846cc1866271deb9f1ef
Jiho <wlgh325@naver.com>
2020-04-08 17:10:52 +0900

    solve 블록 이동하기(BFS)

--- 2020 카카오 블라인드 채용 코딩 테스트/블록 이동하기/Main.java
+++ 2020 카카오 블라인드 채용 코딩 테스트/블록 이동하기/Main.java
@@ -1,7 +1,21 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class Main{
+class Info {
+	int x;
+	int y;
+	int dir;
+	int time;
+	
+	Info(int x, int y, int dir, int time){
+		this.x = x;
+		this.y = y;
+		this.dir = dir;
+		this.time = time;
+	}
+}
 
+class Main {
     public static void main(String[] args){
         int[][] board = {{0,0,0,1,1},
                         {0,0,0,1,0},
@@ -9,137 +23,216 @@ class Main{
                         {1,1,0,0,1},
                         {0,0,0,0,0}};
 
-        int answer = solution(board);
-    }
-
-    // 로봇 크기 : 2x1
-    // 가장 왼쪽 상단: 1,1
-
-    // 0 : 빈칸
-    // 1: 벽
-
-    // 90도 회전 가능
-    // 회전하는데 1초 걸림
-    // dir 상하좌우, 0123
-    // 오르쪽 축으로 위 아래 45
-    // 왼쪽 축으로 위 아래 67
-    static int robotX = 0;
-    static int robotY = 0;
-
-    static int dir = 3; // 상하좌우
-    public static int solution(int[][] board) {        
-        int answer = 0;
-        
-        bfs(board);
-        return answer;
-    }
-
-    public static int bfs(int[][] board){
-        ArrayList<Integer> commandList = find(board);
-        return 0;
+        System.out.println(solution(board));
     }
     
-    public static ArrayList<Integer> find(int[][] board){
-        ArrayList<Integer> commandList = new ArrayList<>();
-
-        // 상하좌우 확인하기{
-        switch(dir){
-            case 0:
-                // 위쪽
-                if(robotX-1 >0){
-                    if(board[robotX-2][robotY] != 0)
-                        commandList.add(0);
-                }
-                // 아래쪽
-                if(robotX < board.length - 1){
-                    if(board[robotX+1][robotY] != 0)
-                        commandList.add(1);
-                }
-                // 왼쪽
-                if(robotY >= 1){
-                    if(board[robotX][robotY-1] != 0 && board[robotX][robotY-1] != 0)
-                        commandList.add(2);
-                }
-                // 오른쪽
-                if(robotY < board.length - 1){
-                    if(board[robotX][robotY+1] != 0 && board[robotX][robotY+1] != 0)
-                        commandList.add(3);
-                }
-                break;
-            case 1:
-                // 위쪽
-                if(robotX > 0){
-                    if(board[robotX-1][robotY] != 0)
-                        commandList.add(0);
-                }
-                // 아래쪽
-                if(robotX +1 < board.length - 1){
-                    if(board[robotX-1-1][robotY] != 0)
-                        commandList.add(1);
-                }
-                // 왼쪽 확인
-                if(robotY-1 >= 0){
-                    if(board[robotX][robotY-1] != 0 && board[robotX+1][robotY-1] != 0)
-                        commandList.add(2);
-                }
-                // 오른쪽 확인
-                if(robotY +1 < board.length - 1){
-                    if(board[robotX][robotY+1] != 0 && board[robotX+1][robotY+1] != 0)
-                        commandList.add(3);
-                }
-                break;
-            case 2:
-                // 위쪽
-                if(robotX > 0){
-                    if(board[robotX-1][robotY-1] != 0 && board[robotX-1][robotY] != 0)
-                        commandList.add(0);
-                }
-                // 아래쪽 확인
-                if(robotX < board.length - 1){
-                    if(board[robotX+1][robotY-1] != 0 && board[robotX+1][robotY] != 0)
-                        commandList.add(1);
-                }
-                // 왼쪽 확인
-                if(robotY-1 >= 0){
-                    if(board[robotX][robotY-1-1] != 0)
-                        commandList.add(2);
-                }
-                // 오른쪽 확인
-                if(robotY < board.length-1){
-                    if(board[robotX][robotY+1] != 0)
-                        commandList.add(3);
-                }
-                break;
-            case 3:
-                // 위쪽 확인
-                if(robotX > 0){
-                    if(board[robotX-1][robotY] != 0 && board[robotX-1][robotY+1] != 0)
-                        commandList.add(0);
-                }
-                // 아래쪽 확인
-                if(robotX < board.length - 1){
-                    if(board[robotX+1][robotY] != 0 && board[robotX+1][robotY+1] != 0)
-                        commandList.add(1);
-                }
-                // 왼쪽 확인
-                if(robotY-1 >= 0){
-                    if(board[robotX][robotY-1] != 0)
-                        commandList.add(2);
-                }
-                // 오른쪽 확인
-                if(robotY < board.length-1){
-                    if(board[robotX][robotY+1+1] != 0)
-                        commandList.add(3);
-                }
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
+    static boolean[][][] visited;
+    // 우, 하, 좌, 상
+    static int[] xdir = {0,1,0,-1};
+    static int[] ydir = {1,0,-1,0};
+    static int N;
+    public static int solution(int[][] board) {
+    	int ans = Integer.MAX_VALUE;
+    	Queue<Info> q = new LinkedList<>();
+    	N = board.length;
+    	visited = new boolean[N][N][4];
+    	
+    	q.offer(new Info(0,0,0,0));
+    	visited[0][0][0] = true;
+        
+        while(!q.isEmpty()) {
+        	Info info = q.poll();
+        	int x = info.x;
+        	int y = info.y;
+        	int dir = info.dir;
+        	int time = info.time;
+        	
+        	int x2 = x + xdir[dir];
+        	int y2 = y + ydir[dir];
+        	
+        	if(x == N-1 && y == N-1 || (x2 == N-1 && y2 == N-1)) {
+        		ans = ans > time ? time : ans;
+        		continue;
+        	}
+        	
+        	int dx=0, dy=0, dx2=0, dy2=0;
+    		for(int i=0; i<4; i++) {
+        		dx = x + xdir[i];
+        		dy = y + ydir[i];
+        		dx2 = x2 + xdir[i];
+        		dy2 = y2 + ydir[i];
+        		
+    			// 유효 범위 면서 방문하지 않았고 벽이 아닌 경우 이동가능
+    			if(isValidPosition(dx,dy) && isValidPosition(dx2,dy2) && board[dx][dy] != 1 && board[dx2][dy2] != 1) {
+    				if(!visited[dx][dy][dir]) {
+    					visited[dx][dy][dir] = true;
+    					q.offer(new Info(dx,dy,dir,time+1));    					
+    				}
+    			}
+			}
+    		
+    		// (x,y) 기준 회전
+    		for(int i=0; i<2; i++) {
+    			int rx = 0;
+    			int ry = 0;
+    			int ddir = 0;
+    			// 가로
+    			if(dir%2 == 0) {
+    				if(i==0) {
+    					ddir = 3;
+    					// 0-> 3
+    					if(dir == 0) {
+    						rx = x-1;
+        					ry = y+1;
+    					}
+    					else if(dir == 2) {
+    						// 2->3
+    						rx = x-1;
+    						ry = y-1;
+    					}
+    				}
+    				else {
+    					ddir = 1;
+    					if(dir == 0) {
+    						// 0 ->1
+    						rx = x+1;
+        					ry = y+1;	
+    					}
+    					else if(dir == 2) {
+    						// 2->1
+    						rx = x+1;
+    						ry = y-1;
+    					}
+    				}
+    			}
+    			else {
+    				if(i==0) {
+    					ddir = 2;
+    					// 1-> 2
+    					if(dir == 1) {
+    						rx = x+1;
+        					ry = y-1;
+    					}
+    					else {
+    						// 3->2
+    						rx = x-1;
+    						ry = y-1;
+    					}
+    				}
+    				else {
+    					ddir = 0;
+    					if(dir == 1) {
+    						// 1->0
+    						rx = x+1;
+        					ry = y+1;	
+    					}
+    					else {
+    						// 3->0
+    						rx = x-1;
+    						ry = y+1;
+    					}
+    				}
+    			}
+    			if(!isValidPosition(rx, ry)) continue;
+    			if(board[rx][ry] == 1) continue;
+    			
+    			dx2 = x + xdir[ddir];
+    			dy2 = y + ydir[ddir];
+    			if(isValidPosition(x, y) && isValidPosition(dx2, dy2) && board[x][y] != 1 && board[dx2][dy2] != 1) {
+    				if(!visited[x][y][ddir]) {
+    					visited[x][y][ddir] = true;
+    					q.offer(new Info(x,y,ddir,time+1));
+    				}
+    			}
+    		}
+    		
+    		dx2 = x + xdir[dir];
+    		dy2 = y + ydir[dir];
+    		dir = (dir+2) % 4;	// 방향 반대로
+    		for(int i=0; i<2; i++) {
+    			int ddir = 0;
+    			int rx=0, ry=0;
+    			
+    			// 가로
+    			if(dir%2 == 0) {
+    				// 시계
+    				if(i==0) {
+    					ddir = 3;
+    					if(dir == 0) {
+    						// 0->3
+    						rx = dx2-1;
+    						ry = dy2+1;
+    					}
+    					else {
+    						// 2->3
+    						rx = dx2-1;
+        					ry = dy2-1;
+    					}
+    				}
+    				else {
+    					ddir = 1;
+    					if(dir == 0) {
+    						// 0->1
+    						rx = dx2+1;
+    						ry = dy2+1;
+    					}
+    					else {
+    						// 2->1
+    						rx = dx2+1;
+        					ry = dy2-1;	
+    					}
+    					
+    				}
+    			}
+    			else {
+    				if(i==0) {
+    					ddir = 2;
+    					if(dir == 1) {
+    						// 1->2
+    						rx = dx2+1;
+    						ry = dy2-1;
+    					}
+    					else {
+    						// 3->2
+    						rx = dx2-1;
+        					ry = dy2-1;	
+    					}
+    				}
+    				else {
+    					ddir = 0;
+    					if(dir == 1) {
+    						// 1 -> 0
+    						rx = dx2+1;
+    						ry = dy2+1;
+    					}
+    					else {
+    						// 3 -> 0
+    						rx = dx2-1;
+        					ry = dy2+1;
+    					}
+    				}
+    			}
+    			
+    			if(!isValidPosition(rx, ry)) continue;
+    			if(board[rx][ry] == 1) continue;
+    			
+    			dx = dx2 + xdir[ddir];
+    			dy = dy2 + ydir[ddir];
+    			
+    			if(isValidPosition(dx, dy) && isValidPosition(dx2, dy2) && board[dx][dy] != 1 && board[dx2][dy2] != 1) {
+    				if(!visited[dx2][dy2][ddir]) {
+    					visited[dx2][dy2][ddir] = true;
+    					q.offer(new Info(dx2,dy2,ddir,time+1));
+    				}
+    			}
+    		}
         }
-        return commandList;
+        
+        return ans; 
+    }
+    
+    private static boolean isValidPosition(int x, int y) {
+    	if(x < 0 || y < 0 || x >= N || y >= N) return false;
+    	return true;
     }
 }
